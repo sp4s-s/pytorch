f9f3da5d56477c686ab1ac7311bb99095768679a
Bin Bao <binbao@meta.com>
2026-01-29 08:16:43 -0800

    [inductor][refactor] Pass inductor_meta through kernel call chain
    Summary: Propagate inductor_meta information, to prepare for upcoming lazy kernel compilation in the cpp-wrapper mode.
    
    ghstack-source-id: 1e29f20bc10a5a88c61a71f07623861625267a55
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/173102

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -5499,6 +5499,7 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
             triton_meta["constants"][signature[arg_num].name] = 1  # type: ignore[index,union-attr]
 
         self.triton_meta = triton_meta
+        self.inductor_meta = inductor_meta
 
         self.codegen_prologue(self.body)
         self.codegen_body()
@@ -5683,6 +5684,7 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
             triton=True,
             arg_types=arg_types,
             triton_meta=self.triton_meta,
+            inductor_meta=self.inductor_meta,
         )
 
         if deallocate_ws:
